d7f784222a8db8757e98c0c8fa2ce892fd000df2
Đặng Văn Trường <developervn@icloud.com>
2019-05-17 18:10:59 +0700

    Create project.

---
+++ CocoaTouchFramework/CocoaTouchFramework.h
@@ -0,0 +1,30 @@
+//
+//                         __   _,--="=--,_   __
+//                        /  \."    .-.    "./  \
+//                       /  ,/  _   : :   _  \/` \
+//                       \  `| /o\  :_:  /o\ |\__/
+//                        `-'| :="~` _ `~"=: |
+//                           \`     (_)     `/
+//                    .-"-.   \      |      /   .-"-.
+//.------------------{     }--|  /,.-'-.,\  |--{     }-----------------.
+//                   (_)_)_)  \_/`~-===-~`\_/  (_(_(_)
+//
+//  CocoaTouchFramework.h
+//  CocoaTouchFramework
+//
+//  Created by Đặng Văn Trường on 17/05/2019.
+//  Copyright (c) 2019 Đặng Văn Trường. All rights reserved.
+//
+    
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CocoaTouchFramework.
+FOUNDATION_EXPORT double CocoaTouchFrameworkVersionNumber;
+
+//! Project version string for CocoaTouchFramework.
+FOUNDATION_EXPORT const unsigned char CocoaTouchFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CocoaTouchFramework/PublicHeader.h>
+
+
---
+++ CocoaTouchFrameworkTests/CocoaTouchFrameworkTests.m
@@ -0,0 +1,48 @@
+//
+//                         __   _,--="=--,_   __
+//                        /  \."    .-.    "./  \
+//                       /  ,/  _   : :   _  \/` \
+//                       \  `| /o\  :_:  /o\ |\__/
+//                        `-'| :="~` _ `~"=: |
+//                           \`     (_)     `/
+//                    .-"-.   \      |      /   .-"-.
+//.------------------{     }--|  /,.-'-.,\  |--{     }-----------------.
+//                   (_)_)_)  \_/`~-===-~`\_/  (_(_(_)
+//
+//  CocoaTouchFrameworkTests.m
+//  CocoaTouchFrameworkTests
+//
+//  Created by Đặng Văn Trường on 17/05/2019.
+//  Copyright (c) 2019 Đặng Văn Trường. All rights reserved.
+//
+    
+
+#import <XCTest/XCTest.h>
+
+@interface CocoaTouchFrameworkTests : XCTestCase
+
+@end
+
+@implementation CocoaTouchFrameworkTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
